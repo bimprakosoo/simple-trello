7a861b850de1e642b73a93e30971d2ebfb69b7a7
Bimo Prakoso <biimoprakosoo@gmail.com>
2023-07-30 17:53:12 +0700

    Add function to add new card

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { DragDropContext, DropResult, Droppable, Draggable } from "react-beautiful-dnd";
-import Column from './Column';
+import Column from "@/components/Column";
 import dynamic from "next/dynamic";
 
 interface Card {
@@ -25,7 +25,10 @@ const initialColumns: Column[] = [
   ];
 
 const Board: React.FC = () => {
-  const [columns, setColumns] = useState<Column>(initialColumns);
+  const [columns, setColumns] = useState<Column[]>(initialColumns);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [newCardDescription, setNewCardDescription] = useState<string>('');
+
   const handleDragEnd = (result: DropResult) => {
     const { source, destination } = result;
 
@@ -52,38 +55,111 @@ const Board: React.FC = () => {
     setColumns([...columns]);
   };
 
+  const handleAddCard = () => {
+    setShowModal(true);
+  }
+
+  const handleModalInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewCardDescription(event.target.value);
+  };
+
+  const handleModalSubmit = () => {
+    if (newCardDescription.trim() !== '') {
+      const newCard: Card = { id: Date.now().toString(), text: newCardDescription };
+      const todoColumnIndex = columns.findIndex((column) => column.id === 'todo');
+
+      if (todoColumnIndex !== -1) {
+        const updatedTodoColumn = {
+          ...columns[todoColumnIndex],
+          cards: [...columns[todoColumnIndex].cards, newCard],
+        };
+
+        const updatedColumns = [...columns];
+        updatedColumns[todoColumnIndex] = updatedTodoColumn;
+
+        setColumns(updatedColumns);
+      }
+    }
+
+    setNewCardDescription('');
+    setShowModal(false);
+  };
+
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        {columns.map((column) => (
-          <Droppable droppableId={column.id} key={column.id}>
-            {(provided) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                style={{
-                  border: '1px solid gray',
-                  borderRadius: '4px',
-                  padding: '8px',
-                  margin: '8px',
-                  minWidth: '200px',
-                  minHeight: '300px',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                }}
-              >
-                <h3>{column.title}</h3>
-                {column.cards.map((card, index) => (
-                  <DynamicDraggable key={card.id} draggableId={card.id} index={index} text={card.text} />
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        ))}
-      </div>
-    </DragDropContext>
+    <>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+          {columns.map((column) => (
+            <Droppable droppableId={column.id} key={column.id}>
+              {(provided) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  style={{
+                    border: '1px solid gray',
+                    borderRadius: '4px',
+                    padding: '8px',
+                    margin: '8px',
+                    minWidth: '200px',
+                    minHeight: '300px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                  }}
+                >
+                  <h3>{column.title}</h3>
+                  {column.cards.map((card, index) => (
+                    <DynamicDraggable key={card.id} draggableId={card.id} index={index} text={card.text} />
+                  ))}
+                  {provided.placeholder}
+                  {column.id === 'todo' && (
+                    <button onClick={handleAddCard} style={{ marginTop: '8px' }}>
+                      + Add a card
+                    </button>
+                  )}
+                </div>
+              )}
+            </Droppable>
+          ))}
+        </div>
+      </DragDropContext>
+
+      {/* Modal */}
+      {showModal && (
+        <div
+          style={{
+            position: 'fixed',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+            background: 'rgba(0, 0, 0, 0.5)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <div
+            style={{
+              background: 'white',
+              padding: '20px',
+              borderRadius: '4px',
+              width: '300px',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+            }}
+          >
+            <input
+              type="text"
+              value={newCardDescription}
+              onChange={handleModalInputChange}
+              placeholder="Enter description..."
+              style={{ marginBottom: '10px', width: '100%', padding: '8px', borderRadius: '4px' }}
+            />
+            <button onClick={handleModalSubmit}>Add Card</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Draggable } from "react-beautiful-dnd";
+import React, { useState } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 
 interface DraggableCardProps {
   draggableId: string;
@@ -8,9 +8,15 @@ interface DraggableCardProps {
 }
 
 const DraggableCard: React.FC<DraggableCardProps> = ({ draggableId, index, text }) => {
+  const [cardText, setCardText] = useState(text);
+
+  const handleChange = (event: React.ChangeEvent<HTMLDivElement>) => {
+    setCardText(event.target.innerText);
+  };
+
   return (
     <Draggable draggableId={draggableId} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <div
           ref={provided.innerRef}
           {...provided.draggableProps}
@@ -19,18 +25,18 @@ const DraggableCard: React.FC<DraggableCardProps> = ({ draggableId, index, text 
             userSelect: 'none',
             padding: '16px',
             margin: '8px',
-            backgroundColor: 'white',
+            backgroundColor: snapshot.isDragging ? 'lightblue' : 'white',
             border: '1px solid gray',
             borderRadius: '4px',
             width: '100%',
             ...provided.draggableProps.style,
           }}
         >
-          {text}
+          {cardText}
         </div>
       )}
     </Draggable>
   );
-}
+};
 
 export default DraggableCard;
